e4841672ec45d76cabcdc0e0f705acb21279d0d4
Bhuvana <bhuvana.maganti@myob.com>
2021-09-17 16:00:06 +1000

    Second unit test - Red

--- TicTacToeTests/UnitTest1.cs
+++ TicTacToeTests/UnitTest1.cs
@@ -12,5 +12,14 @@ namespace TicTacToeTests
             var result = board.GetCells();
             Assert.Equal(result, new char[,]{{'.', '.', '.'}, {'.', '.', '.'}, {'.', '.', '.'}});
         }
+        
+        [Fact]
+        public void Player1_selecting_empty_slot_on_board_changes_the_slot_with_player1_token()
+        {
+            Board board = new Board();
+            Board.Move(1, 1);
+            var result = board.GetCells();
+            Assert.Equal(result, new char[3,3] { {'X', '.', '.'}, {'.', '.', '.'}, {'.', '.', '.'}});
+        }
     }
 }
